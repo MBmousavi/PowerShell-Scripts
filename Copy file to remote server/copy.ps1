17570b0fe279da5992324489317bda263e5b3e73
Mohammad Mousavi <38520491+MBmousavi@users.noreply.github.com>
2021-09-29 12:30:25 +0330

    Update copy.ps1

--- Copy file to remote server/copy.ps1
+++ Copy file to remote server/copy.ps1
@@ -1,6 +1,6 @@
 $date = (get-date).AddDays(-1)
 
-$var = get-childitem C:\path\to\\logs\* | where-object {$_.LastWriteTime -le $date}
+$var = get-childitem C:\path\to\logs\* | where-object {$_.LastWriteTime -le $date}
 	foreach ($f in $var)
 	{
 		pscp.exe -pw password $f user@x.x.x.x:D:\logs\archive
