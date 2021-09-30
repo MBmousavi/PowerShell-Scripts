d40160e3d7dece3832d7d911f5100dfd5f075954
Mohammad Mousavi <38520491+MBmousavi@users.noreply.github.com>
2021-09-30 12:10:42 +0330

    Update copy.ps1

--- Copy file to remote server/copy.ps1
+++ Copy file to remote server/copy.ps1
@@ -3,5 +3,5 @@ $date = (get-date).AddDays(-1)
 $var = get-childitem C:\path\to\logs\* | where-object {$_.LastWriteTime -le $date}
 	foreach ($f in $var)
 	{
-		pscp.exe -pw password $f user@x.x.x.x:D:\logs\archive
+		pscp.exe -pw password $f user@x.x.x.x:D:\logs\archive\ ; if ($?) { Remove-Item  $f }
 	}
